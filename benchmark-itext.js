5ddbfae0c1ff2677449388f3ffe115abb5b14df2
Balearica <admin@scribeocr.com>
2024-05-06 00:27:17 -0700

    Updated to include exact commits before/after issues

--- benchmark-itext.js
+++ benchmark-itext.js
@@ -1,15 +1,19 @@
 // Import Fabric.js
 import * as fabric6 from './dist-v6/index.min.mjs';
+import * as fabric196bea1 from './dist-196bea1/index.mjs';
+import * as fabric26ed225 from './dist-26ed225/index.mjs';
 
 const runs = 5;
 
 const time1Arr = [];
 const time2Arr = [];
+const time3Arr = [];
+const time4Arr = [];
 
-const createCanvasWithIText = (fabric, canvasId, run) => {
+const createCanvasWithIText = (module, canvasId, dispose = true) => {
     const time1 = performance.now();
     // Create the canvas object
-    const canvas = new fabric.Canvas(canvasId, {
+    const canvas = new module.Canvas(canvasId, {
         width: 2500,
         height: 3500
     });
@@ -18,7 +22,7 @@ const createCanvasWithIText = (fabric, canvasId, run) => {
     // Create IText objects with different strings
     for (let i = 0; i < 600; i++) {
         const text = `Text ${i + 1}`;
-        const itext = new fabric.IText(text, {
+        const itext = new module.IText(text, {
             left: (i % 10) * 200, 
             top: Math.floor(i / 10) * 40, 
             fontSize: 24,
@@ -31,16 +35,22 @@ const createCanvasWithIText = (fabric, canvasId, run) => {
     canvas.renderAll();
     const time2 = performance.now();
 
-    if (run + 1 < runs) canvas.dispose();
+    if (dispose) canvas.dispose();
     return time2 - time1;
 };
 
 for (let i=0; i<runs; i++) {
-    time1Arr.push(createCanvasWithIText(fabric, 'canvas1', i));
-    time2Arr.push(createCanvasWithIText(fabric6, 'canvas2', i));
+    time1Arr.push(createCanvasWithIText(fabric, 'canvas1'));
+    time2Arr.push(createCanvasWithIText(fabric6, 'canvas1'));
+    time3Arr.push(createCanvasWithIText(fabric196bea1, 'canvas1'));
+    time4Arr.push(createCanvasWithIText(fabric26ed225, 'canvas1', i + 1 < runs));
 }
 
 const mean = array => Math.round(array.reduce((a, b) => a + b) / array.length * 1e3) / 1e3;
 const time1 = mean(time1Arr);
 const time2 = mean(time2Arr);
+const time3 = mean(time3Arr);
+const time4 = mean(time4Arr);
+
 console.log(`Mean time v5: ${time1} ms vs. v6: ${time2} ms.`);
+console.log(`Mean time 196bea1: ${time3} ms vs. 26ed225: ${time4} ms.`);
